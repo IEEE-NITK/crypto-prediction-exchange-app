a7a3215b0fcc36b74cadad865300b0476ef54928
predator4hack <chandankum2440@gmail.com>
2022-03-30 00:44:21 +0530

    data from websockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,67 @@
-const express = require("express");
-// const http = require("http");
-// const socketio = require("socket.io");
-// const path = require("path");
+const WebSocket = require("ws");
 
-// const app = express();
-// const server = http.createServer(app);
-// const io = socketio(server);
-const { io } = require("socket.io-client");
+let streams = [
+    "ethbtc@miniTicker",
+    "bnbbtc@miniTicker",
+    "wavesbtc@miniTicker",
+    "bchabcbtc@miniTicker",
+    "bchsvbtc@miniTicker",
+    "xrpbtc@miniTicker",
+    "tusdbtc@miniTicker",
+    "eosbtc@miniTicker",
+    "trxbtc@miniTicker",
+    "ltcbtc@miniTicker",
+    "xlmbtc@miniTicker",
+    "bcptbtc@miniTicker",
+    "adabtc@miniTicker",
+    "zilbtc@miniTicker",
+    "xmrbtc@miniTicker",
+    "stratbtc@miniTicker",
+    "zecbtc@miniTicker",
+    "qkcbtc@miniTicker",
+    "neobtc@miniTicker",
+    "dashbtc@miniTicker",
+    "zrxbtc@miniTicker",
+];
 
-const port = process.env.PORT || 3000;
+let trackedStreams = [];
 
-server.listen(port, () => {
-    console.log(`Server is up on ${port}!`);
-});
+//let ws = new WebSocket("wss://stream.binance.com:9443/ws/" + streams.join('/'));
+let ws = new WebSocket("wss://stream.binance.com:9443/ws/!miniTicker@arr");
+
+ws.onopen = function () {
+    console.log("Binance connected...");
+};
+
+ws.onmessage = function (evt) {
+    try {
+        let msgs = JSON.parse(evt.data);
+        // if (Array.isArray(msgs)) {
+        //     for (let msg of msgs) {
+        //         handleMessage(msg);
+        //     }
+        // } else {
+        //     handleMessage(msgs);
+        // }
+        console.log(msgs);
+    } catch (e) {
+        console.log("Unknown message: " + evt.data, e);
+    }
+};
+
+ws.onclose = function () {
+    console.log("Binance disconnected");
+};
+
+function handleMessage(msg) {
+    const stream = msg.s;
+    if (trackedStreams.indexOf(stream) === -1) {
+        document.getElementById("streams").innerHTML +=
+            "<br/>" + stream + ': <span id="stream_' + stream + '"></span>';
+        trackedStreams.push(stream);
+        document.getElementById("totalstreams").innerText =
+            trackedStreams.length;
+    }
+
+    document.getElementById("stream_" + stream).innerText = msg.v;
+}
